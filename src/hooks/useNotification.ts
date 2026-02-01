07e3cfaf2423d95d9cb9bc9f98b2f5ab1689765b
Diego Cáceres Cardoza <diegokaiser@gmail.com>
2026-02-01 16:27:32 +0100

    fix type hooks/useNotification

--- src/hooks/useNotification.ts
+++ src/hooks/useNotification.ts
@@ -1,6 +1,7 @@
 import apis from '@/apis';
 import { Notification, NotificationDB } from '@/types/notification';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { Timestamp } from 'firebase/firestore';
 
 export const usePostNotification = () => {
   const queryClient = useQueryClient();
@@ -20,8 +21,13 @@ export const usePatchNotification = () => {
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationFn: ({ uid, data }: { uid: string; data: Partial<Notification> }) =>
-      apis.notification.PatchNotification(uid, data),
+    mutationFn: ({ uid, data }: { uid: string; data: Partial<Notification> }) => {
+      const parsedData = {
+        ...data,
+        hour: data.hour ? Timestamp.fromDate(new Date(data.hour)) : undefined,
+      };
+      return apis.notification.PatchNotification(uid, parsedData as Partial<NotificationDB>);
+    },
     onSuccess: () => {
       queryClient.invalidateQueries({
         queryKey: ['notifications'],
