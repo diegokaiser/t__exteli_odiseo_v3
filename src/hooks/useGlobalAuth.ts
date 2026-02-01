c6a7592778a765adae4adaebb3ac9d18d1956e59
Diego Cáceres Cardoza <diegokaiser@gmail.com>
2026-02-02 00:00:21 +0100

    fix landing unauth

--- src/hooks/useGlobalAuth.ts
+++ src/hooks/useGlobalAuth.ts
@@ -1,5 +1,5 @@
 import { useAuth } from '@/auth/hooks/useAuth';
 
 export function useGlobalAuth() {
-  return useAuth('/dashboard', '/login', ['/login', '/forgot-password']);
+  return useAuth('/dashboard', '/login');
 }
