220f6e47d008c4a9bb5877ef118607c17b06a797
Diego Cáceres Cardoza <diegokaiser@gmail.com>
2026-02-02 23:54:55 +0100

    fix no iframe para calendar/reserva

---
+++ src/app/(landings)/layout.tsx
@@ -0,0 +1,7 @@
+import { LandingWrapper } from '@/components/wrappers';
+
+const LandingsLayout = ({ children }: { children: React.ReactElement }) => {
+  return <LandingWrapper>{children}</LandingWrapper>;
+};
+
+export default LandingsLayout;
--- src/components/wrappers/index.tsx
+++ src/components/wrappers/index.tsx
@@ -1,3 +1,4 @@
 export { default as DashboardWrapper } from './Dashboard';
 export { default as Document } from './Document';
+export { default as LandingWrapper } from './Landing';
 export { default as Report } from './Report';
