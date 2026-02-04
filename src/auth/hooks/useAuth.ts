7f5b182f8aebd49dec21f4e2701baa5eb7d50fec
Diego Cáceres Cardoza <diegokaiser@gmail.com>
2026-02-04 14:42:40 +0100

    delete storage and session in login y logout

--- src/auth/hooks/useAuth.ts
+++ src/auth/hooks/useAuth.ts
@@ -5,10 +5,11 @@ import { Models } from 'appwrite';
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
+const ALLOW_UNAUTHENTICATED = ['/landings', '/calendar/reserva', '/api/stripe'];
+
 export const useAuth = (
   redirectAuthenticated = '/dashboard',
-  redirectUnauthenticated = '/login',
-  allowUnauthenticated: string[] = []
+  redirectUnauthenticated = '/login'
 ) => {
   const [user, setUser] = useState<Models.User<{}> | null>(null);
   const [loading, setLoading] = useState(true);
@@ -21,7 +22,7 @@ export const useAuth = (
         setUser(currentUser);
 
         const currentPath = window.location.pathname;
-        const isPublicRoute = allowUnauthenticated.includes(currentPath);
+        const isPublicRoute = ALLOW_UNAUTHENTICATED.some((path) => currentPath.startsWith(path));
 
         if (isPublicRoute) {
           router.replace(redirectAuthenticated);
@@ -31,7 +32,7 @@ export const useAuth = (
         setUser(null);
 
         const currentPath = window.location.pathname;
-        const isPublicRoute = allowUnauthenticated.includes(currentPath);
+        const isPublicRoute = ALLOW_UNAUTHENTICATED.some((path) => currentPath.startsWith(path));
 
         if (!isPublicRoute) {
           router.replace(redirectUnauthenticated);
@@ -44,6 +45,11 @@ export const useAuth = (
   }, []);
 
   const login = async (email: string, password: string, remember: boolean) => {
+    try {
+      await account.deleteSession('current');
+    } catch (error: any) {
+      if (error.code !== 401) console.error(error);
+    }
     await account.createEmailPasswordSession(email, password);
     if (remember) {
       localStorage.setItem('lastUser', JSON.stringify({ email, password }));
@@ -57,7 +63,12 @@ export const useAuth = (
   };
 
   const logout = async () => {
-    await account.deleteSession('current');
+    try {
+      await account.deleteSession('current');
+    } catch (err) {
+      console.error(err);
+    }
+    localStorage.removeItem('lastUser');
     setUser(null);
     router.push(redirectUnauthenticated);
   };
